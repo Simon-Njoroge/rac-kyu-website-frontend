e216f90680e5e8fba397896307cc227996e5ad3c
simon njoroge <mukirisimon22@gmail.com>
2024-08-07 18:49:14 +0300

    rac kyu frontend

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -25,9 +25,9 @@ const Projects = () => {
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 my-10 px-4 md:px-10">
                 {
                     projects && projects.map((project: any) => (
-                        <div key={project.id} className="relative w-full h-48 group overflow-hidden rounded-md shadow-md transition-transform duration-300 hover:scale-105">
-                            <img src={project.image} alt="" className="w-full h-full object-cover" />
-                            <div className="absolute inset-0 flex flex-col justify-center items-center text-white bg-black bg-opacity-60 opacity-0 group-hover:opacity-100 transition-opacity duration-300 p-4">
+                        <div key={project.id} className="relative w-full h-60 group overflow-hidden rounded-md shadow-md transition-transform duration-300 hover:scale-105">
+                            <img src={project.image} alt="" className="w-full h-48 object-cover" />
+                            <div className="absolute bottom-0 left-0 w-full bg-black bg-opacity-60 text-white p-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                                 <p className="text-center text-lg font-semibold mb-2">{project.description}</p>
                                 <p className="text-center text-sm">{project.date}</p>
                             </div>
