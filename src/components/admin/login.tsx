610e83fdc955805b9a859e448e9196f09100361d
simon njoroge <mukirisimon22@gmail.com>
2024-09-19 15:29:50 +0300

    rac kyu frontend

--- src/components/admin/login.tsx
+++ src/components/admin/login.tsx
@@ -14,7 +14,7 @@ const Login = () => {
     setLoading(true);
 
     try {
-      const response = await axios.post('/api/login', { email, password });
+      const response = await axios.post('/login', { email, password });
       
      
       const { token } = response.data;
