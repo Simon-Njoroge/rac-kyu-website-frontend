65314aa320cf6e04f677bfdc0c1319ec8e551a11
simon njoroge <mukirisimon22@gmail.com>
2024-09-26 15:23:02 +0300

    rac kyu frontend

--- src/components/blogs.tsx
+++ src/components/blogs.tsx
@@ -1,57 +1,71 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { api } from './home';
+import { FadeLoader } from 'react-spinners';
 
-import axios from 'axios'
-import { useState, useEffect } from 'react'
-import { api } from './home'
-import { FadeLoader } from "react-spinners";
 interface Tblog {
-  id:number,
-  title:string,
-  image:string,
-  descrition:string
+  id: number;
+  title: string;
+  image: string;
+  descrition: string;
 }
+
 const Blogs = () => {
-  const [blogs,setBlogs]=useState<Tblog[]>([])
-  const Handleblogdata=async()=>{
-    try{
-      const res= await axios.get(`${api}/allblogs`)
-      setBlogs(res.data)
+  const [blogs, setBlogs] = useState<Tblog[]>([]);
+
+  const Handleblogdata = async () => {
+    try {
+      const res = await axios.get(`${api}/allblogs`);
+      setBlogs(res.data);
+    } catch (error) {
+      console.error('failed to load blogs data', error);
     }
-    catch(error){
-      console.error("failed to load blogs data",error)
-    }
-  }
-  useEffect(()=>{
-    Handleblogdata()
-  },[])
- console.log(blogs)
+  };
+
+  useEffect(() => {
+    Handleblogdata();
+  }, []);
+
+  console.log(blogs);
+
   return (
     <>
-    <div>
-    <p className="bg-pink-600 text-center h-20 flex items-center mt-20 mx-10 justify-center text-white font-bold text-xl">
+      <div>
+        <p className="bg-pink-600 text-center h-20 flex items-center mt-20 mx-10 justify-center text-white font-bold text-xl">
           Blogs
         </p>
-    </div>
-    <div className='grid grid-cols-3 gap-5 mx-10 mt-10 '>
-      {
-        blogs.length > 0 ?(blogs && blogs.map((blog:Tblog)=>{
-          return(<>
-           <div key={blog.id} className='cursor-pointer relative hover:-top-5 hover:shadow-md   hover:shadow-black rounded-md hover:ease-in-out'>
-            <img src={blog.image} alt="" className='w-full h-48 rounded-md'/>
-            <h1 className='my-5 text-center font-bold   '>{blog.title.toUpperCase()}</h1>
-            <p className=' p-2'>{blog.descrition}</p>
-            </div>
-            </>)
-        })):(
-          <div className="w-full items-center justify-center gap-5 flex">
-          <FadeLoader  color="#ff007f" />
-          
-          <p className="mt-3">loading blogs...</p>
-        </div>
-        )
-      }
-    </div>
-    </>
-  )
-}
+      </div>
 
-export default Blogs
+      {/* Grid layout for blogs */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 mx-5 sm:mx-10 mt-10">
+        {blogs.length > 0 ? (
+          blogs.map((blog: Tblog) => {
+            return (
+              <div
+                key={blog.id}
+                className="cursor-pointer relative hover:-top-5 hover:shadow-md hover:shadow-black rounded-md hover:ease-in-out p-4 bg-white"
+              >
+                <img
+                  src={blog.image}
+                  alt={blog.title}
+                  className="w-full h-48 object-cover rounded-md"
+                />
+                <h1 className="my-5 text-center font-bold text-lg">
+                  {blog.title.toUpperCase()}
+                </h1>
+                <p className="p-2 text-justify">{blog.descrition}</p>
+              </div>
+            );
+          })
+        ) : (
+          <div className="w-full flex flex-col items-center justify-center gap-5">
+            <FadeLoader color="#ff007f" />
+            <p className="mt-3">Loading blogs...</p>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Blogs;
